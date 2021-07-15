966ce43331236e07968832c1e66c0ccee93364e3
timothypidashev <pidashev.tim@gmail.com>
2021-07-15 11:03:59 -0700

    upload config

--- venv/index.py
+++ venv/index.py
@@ -8,8 +8,3 @@ app = Flask(__name__)
 def home():
     return "Home Page Route"
 
-@app.route("/api")
-def api():
-    with open("data.json", mode="r") as my_file:
-        text = my_file.read()
-        return text
