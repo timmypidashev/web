b0cbf9142df3ccf2bb3ec69591fb41b342e7078b
timothypidashev <pidashev.tim@gmail.com>
2021-07-15 02:38:09 +0000

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Timothy Pidashev',
+    title: 'Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
