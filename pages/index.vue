2c256b62e16b28ceae5ae8e2d21551355c349dd4
timothypidashev <pidashev.tim@gmail.com>
2021-07-15 04:04:12 +0000

    add more changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,61 @@
 <template>
-  <div>
-    <NavBar />
-    <About />
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        Timothy Pidashev
+      </h1>
+      <h2 class="subtitle">
+        A 16-year-old on an epic journey to become a software developer! 
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/timothypidashev"
+          target="_blank"
+          rel="noopener"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links {
+  padding-top: 15px;
+}
+</style>
