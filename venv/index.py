142129a13f9543f2b76db9344a8f7f2875e425a8
timothypidashev <pidashev.tim@gmail.com>
2021-07-15 11:50:18 -0700

    working version

--- venv/index.py
+++ venv/index.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=".", static_url_path="")
 
 
 @app.route("/")
 def home():
-    return "Home Page Route"
+    return app.send_static_file("index.html")
 
