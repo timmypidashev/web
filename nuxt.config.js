87576c3b76c9a26f79a6b5d1f4a3d6de683987c8
timothypidashev <pidashev.tim@gmail.com>
2021-07-15 02:41:55 +0000

    fix errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/bootstrap.js'],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -30,7 +30,7 @@ export default {
   buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['bootstrap-vue/nuxt'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
