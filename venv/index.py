810ba2252216e38618ed00a66f5942b225a52503
timmypidashev <pidashev.tim@gmail.com>
2021-09-07 14:41:45 -0700

    remove `venv`

--- venv/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__, template_folder = "templates", static_url_path="/static")
-
-#home page
-@app.route("/")
-def index():
-    return render_template("index.html")
