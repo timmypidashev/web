c9385a50b8f7581ee6f799e5f422639fee315778
Timothy Pidashev <pidashev.tim@gmail.com>
2022-01-08 14:20:15 -0800

    push more code

--- js/python/login.js
+++ js/python/login.js
@@ -1,6 +1,15 @@
-$("#login-button").click(function(event){
-    event.preventDefault();
-
-$('form').fadeOut(500);
-$('.wrapper').addClass('form-success');
-});
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "student" && password == "student.23264"){
+        // window.location = "editor.html"; // Redirecting to other page.
+        // properly redirect to editor.html
+        window.location.replace("https://timmypidashev.com/editor.html");
+        return false;
+    }
+    else{
+        alert("Username/Password is incorrect");
+        return false;
+    }
+}
