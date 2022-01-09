3c371383d54f6d23c5865e0b1d799cbac2efe354
Timothy Pidashev <pidashev.tim@gmail.com>
2022-01-08 16:48:15 -0800

    minor polish

--- js/python/login.js
+++
@@ -1,9 +0,0 @@
-function login(username, password) {
-    if (username == 'student' && password == 'student.23264') {
-        setTimeout(function() { // delay the location.replace for one second for login animation
-            location.replace('assignments.html');
-        }, 1000);
-    } else {
-        return false;
-    }
-}
