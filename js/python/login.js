074f13683a5a12f65b47edcca2aa6a38b87681c2
Timothy Pidashev <pidashev.tim@gmail.com>
2022-01-08 16:32:53 -0800

    hopefully deployment is better than dev

--- js/python/login.js
+++ js/python/login.js
@@ -1,15 +1,9 @@
-// Below function Executes on click of login button.
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if ( username == "student" && password == "student.23264"){
-        // window.location = "editor.html"; // Redirecting to other page.
-        // properly redirect to editor.html
-        window.location.replace("https://timmypidashev.com/editor.html");
-        return false;
-    }
-    else{
-        alert("Username/Password is incorrect");
+function login(username, password) {
+    if (username == 'student' && password == 'student.23264') {
+        setTimeout(function() { // delay the location.replace for one second for login animation
+            location.replace('assignments.html');
+        }, 1000);
+    } else {
         return false;
     }
 }
