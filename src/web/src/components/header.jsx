70c7d035767061f070930ec8df157c36d8a99311
Timothy Pidashev <pidashev.tim@gmail.com>
2024-03-18 18:15:33 -0700

    Add font

--- src/web/src/components/header.jsx
+++ src/web/src/components/header.jsx
@@ -2,30 +2,6 @@
 
 import ThemeToggle from "@/components/theme-toggle"
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-const navItems= [
-  {
-    path: "/",
-    name: "Home"
-  },
-  {
-    path: "/projects",
-    name: "Projects"
-  },
-  {
-    path: "/resume",
-    name: "Resume"
-  },
-  {
-    path: "/blog",
-    name: "Blog"
-  },
-  {
-    path: "/shop",
-    name: "Shop"
-  }
-]
 
 const Header = () => {
 
