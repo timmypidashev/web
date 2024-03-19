385b2379066d85cea4a0bf0d0d8173d1aaf699ca
Timothy Pidashev <pidashev.tim@gmail.com>
2024-03-18 18:38:35 -0700

    style not-found

--- src/web/src/app/not-found.jsx
+++ src/web/src/app/not-found.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from "next/link";
 
 const Not_found = () => {
   return (
-    <div>
-      <p>404: The requested page is not found!</p>
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center">
+      <h1 className="font-bold text-light-red-1 dark:text-dark-red-1">Whoops, the requested page could not be found...</h1>
+      <Link href="/" className="text-light-blue-1 dark:text-blue-1 hover:underline">Go back to Home</Link>
     </div>
   )
 }
