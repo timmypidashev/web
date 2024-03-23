8b33094cef9166a1e3586a7d3d5b0d98ccd0ae8c
Timothy Pidashev <pidashev.tim@gmail.com>
2024-03-23 04:02:55 +0000

    Update hero sections

--- src/web/src/components/hero/index.jsx
+++ src/web/src/components/hero/index.jsx
@@ -1,45 +1,16 @@
 "use client"
 
 import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { ScrollControls, Scroll } from '@react-three/drei';
-import * as THREE from 'three';
-import Particles from '@/components/hero/particles';
-import Sections from '@/components/hero/sections';
+import { Section1, Section2, Section3 } from '@/components/hero/sections';
 
 function Hero() {
-  const cameraRef = useRef();
-
   return (
-    <div className="w-full h-screen">
-      <Canvas>
-        <ScrollControls pages={3}>
-          <Scroll>
-            <Particles />
-          </Scroll>
-          <Scroll html>
-            <Sections />
-          </Scroll>
-        </ScrollControls>
-        <perspectiveCamera ref={cameraRef} position={[0, 0, 5]} />
-        <SceneUpdater cameraRef={cameraRef} />
-      </Canvas>
+    <div className="h-[calc(100vh-96px)] relative">
+      <Section1 />
+      <Section2 />
+      <Section3 />
     </div>
   );
-}
-
-function SceneUpdater({ cameraRef }) {
-  useFrame(({ pointer }) => {
-    const camera = cameraRef.current;
-    if (!camera) return;
-
-    camera.position.x = THREE.MathUtils.lerp(camera.position.x, pointer.x * 0.5, 0.03);
-    camera.position.y = THREE.MathUtils.lerp(camera.position.y, pointer.y * 0.8, 0.01);
-    camera.position.z = THREE.MathUtils.lerp(camera.position.z, Math.max(4, Math.abs(pointer.x * pointer.y * 8)), 0.01);
-    camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, pointer.x * -Math.PI * 0.025, 0.001);
-  });
-
-  return null;
 }
 
 export default Hero;
--- src/web/src/components/hero/sections.jsx
+++ src/web/src/components/hero/sections.jsx
@@ -1,19 +1,102 @@
-import React from 'react';
+"use client"
 
-function Sections() {
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import React, { useRef } from 'react';
+
+
+function Section1() {
+  const [ref, inView] = useInView();
+  const containerRef = useRef(null);
+  
   return (
-    <div className="relative h-screen overflow-y-scroll">
-      <div className="h-screen flex items-center justify-center">
-        <h1 className="text-gray-700">hello.</h1>
-      </div>
-      <div className="h-screen flex items-center justify-center">
-        <h1 className="text-orange-400">Your Future</h1>
-      </div>
-      <div className="h-screen flex items-center justify-center">
-        <h1 className="text-indigo-800">Awaits</h1>
+    <div className="relative h-screen overflow-y-scroll font-bold text-4xl">
+      <div className="h-screen flex flex-col items-center justify-center" ref={containerRef}>
+        <motion.div
+          className="text-center"
+          ref={ref}
+          initial={{ opacity: 0, x: -50 }}
+          animate={inView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>Hello, I'm</h1>
+        </motion.div>
+        <motion.div
+          className="text-center"
+          ref={ref}
+          initial={{ opacity: 0, x: -50 }}
+          animate={inView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          <h2>Timothy Pidashev</h2>
+        </motion.div>
       </div>
     </div>
   );
-}
+};
 
-export default Sections;
+function Section2() {
+  const [ref, inView] = useInView();
+  const containerRef = useRef(null);
+
+  return (
+    <div className="relative h-screen overflow-y-scroll font-bold text-4xl">
+      <div className="h-screen flex flex-col items-center justify-center" ref={containerRef}>
+        <motion.div
+          className="text-center"
+          ref={ref}
+          initial={{ opacity: 0, x: -50 }}
+          animate={inView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>I'm a 19 year old</h1>
+        </motion.div>
+        <motion.div
+          className="text-center"
+          ref={ref}
+          initial={{ opacity: 0, x: -50 }}
+          animate={inView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          <h2>On an epic journey</h2>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+function Section3() {
+  const [ref, inView] = useInView();
+  const containerRef = useRef(null);
+  
+  return (
+    <div className="relative h-screen overflow-y-scroll font-bold text-4xl">
+      <div className="h-screen flex flex-col items-center justify-center" ref={containerRef}>
+        <motion.div
+          className="text-center"
+          ref={ref}
+          initial={{ opacity: 0, x: -50 }}
+          animate={inView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>To become</h1>
+        </motion.div>
+        <motion.div
+          className="text-center"
+          ref={ref}
+          initial={{ opacity: 0, x: -50 }}
+          animate={inView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          <h2>A software engineer!</h2>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export { 
+  Section1,
+  Section2,
+  Section3,
+}
