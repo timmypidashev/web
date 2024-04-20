068d2a5c7a73b4c96afd6d7dea87899b82da48b3
Timothy Pidashev <pidashev.tim@gmail.com>
2024-04-20 15:34:23 -0700

    Update lockfile, fix font sizes in not-found

--- src/web/src/app/not-found.jsx
+++ src/web/src/app/not-found.jsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 const Not_found = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center">
-      <h1 className="font-bold text-light-red-1 dark:text-dark-red-1">
-        Whoops, the requested page could not be found...
+      <h1 className="lg:text-4xl md:text-3xl font-bold text-light-red-1 dark:text-dark-red-1">
+        Sorry, this page doesn't exist!
       </h1>
-      <Link href="/" className="text-light-blue-1 dark:text-blue-1 hover:underline">
+      <Link href="/" className="lg:text-4xl md:text-sm text-light-blue-1 dark:text-blue-1 hover:underline">
         Go back to Home
       </Link>
     </div>
