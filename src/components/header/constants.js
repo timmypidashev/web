502b1a93e10cb3058ee988a38b8f3378dd0fe485
Timothy Pidashev <pidashev.tim@gmail.com>
2024-06-10 09:59:42 -0700

    Fix navigation links

--- src/components/header/constants.js
+++ src/components/header/constants.js
@@ -1,10 +1,10 @@
 // Navigation links
 export const Links = [
   { id: 0, href: "/", label: "Home", color: "green" },
-  { id: 1, href: "projects", label: "Projects", color: "yellow" },
-  { id: 2, href: "resume", label: "Resume", color: "blue" },
-  { id: 3, href: "blog", label: "Blog", color: "purple" },
-  { id: 4, href: "shop", label: "Shop", color: "aqua" }
+  { id: 1, href: "/projects", label: "Projects", color: "yellow" },
+  { id: 2, href: "/resume", label: "Resume", color: "blue" },
+  { id: 3, href: "/blog", label: "Blog", color: "purple" },
+  { id: 4, href: "/shop", label: "Shop", color: "aqua" }
 ];
 
 // Drop down menu link colors
