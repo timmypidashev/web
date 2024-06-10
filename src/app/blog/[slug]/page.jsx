efa4be2fd94787143cb71d47e72e958fe8c3a51e
Timothy Pidashev <pidashev.tim@gmail.com>
2024-06-09 17:59:08 -0700

    Colors work on inline code lets go

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,4 +1,6 @@
+// [slug]/page.jsx
 import { getBlogBySlug, getAllBlogSlugs } from "@/lib/mdx";
+import MdxProvider from "@/components/prism/mdx-provider";
 
 export async function generateStaticParams() {
   const slugs = await getAllBlogSlugs();
@@ -9,14 +11,18 @@ export async function generateStaticParams() {
 
 export default async function BlogPage({ params }) {
   const blog = await getBlogBySlug(params.slug);
+
   return (
-    <section className="py-16 prose">
+    <section className="py-16 prose mx-auto">
       <article>
         <h1 className="text-light-foreground dark:text-dark-foreground">{blog.frontmatter.title}</h1>
         <p className="text-light-yellow-1 dark:text-dark-yellow-1">{blog.frontmatter.author}</p>
         <p className="text-light-blue-1 dark:text-dark-blue-1">{blog.frontmatter.date}</p>
-        <div>{blog.content}</div>
+        <MdxProvider>
+          <div>{blog.content}</div>
+        </MdxProvider>
       </article>
     </section>
   );
 }
+
---
+++ src/components/prism/prism-setup.js
@@ -0,0 +1,10 @@
+// prism-setup.js
+import Prism from 'prismjs';
+import 'prismjs/components/prism-python'; // Add more languages as needed
+
+// Add more languages as needed, e.g.:
+// import 'prismjs/components/prism-python';
+// import 'prismjs/components/prism-java';
+
+export default Prism;
+
