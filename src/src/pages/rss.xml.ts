935d2a90772f88003b43628abf67c60611053717
Timothy Pidashev <mail@timmypidashev.dev>
2025-01-23 14:53:26 -0800

    Update dependencies; add rss feed

---
+++ src/src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import type { APIContext } from "astro";
+
+export async function GET(context: APIContext) {
+  const blog = await getCollection("blog");
+  
+  return rss({
+    title: "Timothy Pidashev",
+    description: "My experiences and technical insights into software development and the ever-evolving world of programming.",
+    site: context.site!,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
