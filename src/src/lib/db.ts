37c63db863cd5e959cce8091ba4a49ef268e0ccc
Timothy Pidashev <mail@timmypidashev.dev>
2025-04-10 10:38:44 -0700

    Update dependencies; add scripts submodule

---
+++ src/src/lib/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+globalThis.prisma = db;
